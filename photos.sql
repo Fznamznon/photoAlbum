1d1581e5dffb7aaddb979078e5f4c99a2581f22b
kukanya <kukanya.nya@gmail.com>
2015-12-17 07:02:19 +0300

    update sql dump

--- photos.sql
+++ photos.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Хост: localhost
--- Время создания: Ноя 29 2015 г., 22:15
+-- Время создания: Дек 17 2015 г., 06:16
 -- Версия сервера: 5.6.26
 -- Версия PHP: 5.5.29
 
@@ -23,6 +23,26 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Структура таблицы `albums`
+--
+
+CREATE TABLE `albums` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `description` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `albums`
+--
+
+INSERT INTO `albums` (`id`, `name`, `user_id`, `description`) VALUES
+(1, '123', 2, '456');
+
+-- --------------------------------------------------------
+
+--
 -- Структура таблицы `photo`
 --
 
@@ -30,37 +50,110 @@ CREATE TABLE `photo` (
   `id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
   `description` text NOT NULL,
-  `filename` varchar(255) NOT NULL
+  `filename` varchar(255) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `album_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Дамп данных таблицы `photo`
 --
 
-INSERT INTO `photo` (`id`, `name`, `description`, `filename`) VALUES
-(1, 'ÐºÐ°Ñ€Ñ‚Ð¸Ð½ÐºÐ°', '', 'd45f07914599f01e1b34c63ec3702f0f1444651985.png'),
-(2, 'ÐšÐ«', '', '731a363141904d8524fecd929365526b1444652400.png');
+INSERT INTO `photo` (`id`, `name`, `description`, `filename`, `user_id`, `album_id`) VALUES
+(1, 'ÐºÐ°Ñ€Ñ‚Ð¸Ð½ÐºÐ°', '', 'd45f07914599f01e1b34c63ec3702f0f1444651985.png', 2, NULL),
+(2, 'ÐšÐ«', '', '731a363141904d8524fecd929365526b1444652400.png', 2, NULL),
+(3, 'pokgwopegkp', '', 'a59a1e1fb0484d902b137c5bb4334db11449412963.png', 2, NULL),
+(4, 'utkktrk78', '', 'a59a1e1fb0484d902b137c5bb4334db11449412987.png', 2, NULL),
+(5, '1111111111111111', '', 'c3a3408ef7738d76e21fdb2ad9dcfaef1449413108.jpg', 2, NULL),
+(6, 'aOiT1VhJx3I.jpg', '', '2920ed9d54e7d3d850a29711778c054f1449416557.jpg', 2, NULL),
+(7, 'Screenshot_4.png', '', 'de80923a7846ae4ff4294f0d2e0aa74d1449416775.png', 2, NULL),
+(8, 'lol', '', '3e066956d9868b10fd5682f6a57b04071449416916.png', 2, NULL),
+(9, 'Ñ‹', '', '01e88df767188ed0f9bd3acb373da22a1450193950.jpg', 2, NULL),
+(10, 'ÐºÐ¾Ñ‚Ñ', '', '08e63025a01122bba47c0d737215e8071450203139.JPG', 2, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `login` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `users`
+--
+
+INSERT INTO `users` (`id`, `login`, `password`, `name`) VALUES
+(1, 'test', '40bd001563085fc35165329ea1ff5c5ecbdbbeef', 'test'),
+(2, 'lol', '403926033d001b5279df37cbbe5287b7c7c267fa', 'lol');
 
 --
 -- Индексы сохранённых таблиц
 --
 
 --
+-- Индексы таблицы `albums`
+--
+ALTER TABLE `albums`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
 -- Индексы таблицы `photo`
 --
 ALTER TABLE `photo`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `filename` (`filename`);
+  ADD UNIQUE KEY `filename` (`filename`),
+  ADD KEY `user_id` (`user_id`) USING BTREE,
+  ADD KEY `album_id` (`album_id`);
+
+--
+-- Индексы таблицы `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT для сохранённых таблиц
 --
 
 --
+-- AUTO_INCREMENT для таблицы `albums`
+--
+ALTER TABLE `albums`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
 -- AUTO_INCREMENT для таблицы `photo`
 --
 ALTER TABLE `photo`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+--
+-- AUTO_INCREMENT для таблицы `users`
+--
+ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- Ограничения внешнего ключа сохраненных таблиц
+--
+
+--
+-- Ограничения внешнего ключа таблицы `albums`
+--
+ALTER TABLE `albums`
+  ADD CONSTRAINT `albums_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ограничения внешнего ключа таблицы `photo`
+--
+ALTER TABLE `photo`
+  ADD CONSTRAINT `photo_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `photo_ibfk_2` FOREIGN KEY (`album_id`) REFERENCES `albums` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
